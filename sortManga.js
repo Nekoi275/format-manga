a7ca5e0130e31a255ace306b771177e325d54372
nekoi <nekoi275@gmail.com>
2023-07-03 15:30:18 +0300

    creating chapters sctructure

--- sortManga.js
+++ sortManga.js
@@ -4,8 +4,34 @@ const MANGAFOLDER = '../../Manga';
 const MANGA = 'Ah! My Goddess! Eng (complete)';
 const copyDir = `${MANGAFOLDER}/${MANGA}(copy)`;
 
-fs.cp(`${MANGAFOLDER}/${MANGA}`, copyDir, { recursive: true }, (err) => {
+/* fs.cp(`${MANGAFOLDER}/${MANGA}`, copyDir, { recursive: true }, (err) => {
     if (err) throw err;
     console.log('Successfully copied')
-})
+}) */
 
+function createChapters(dir) {
+    let volumes = fs.readdirSync(dir, { withFileTypes: true });
+    let chapterNumber = 0;
+    for (let i = 0; i < volumes.length; i++) {
+        let chapters = fs.readdirSync(`${dir}/${volumes[i].name}`, { withFileTypes: true });
+        for (let j = 0; j < chapters.length; j++) {
+            fs.rename(`${dir}/${volumes[i].name}/${chapters[j].name}`, `${dir}/ch ${chapterNumber}`, (err) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log("Successfully created the chapter.")
+                }
+            })
+            chapterNumber++;
+        }
+        fs.rmdir(`${dir}/${volumes[i].name}`, (err) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log("Successfully removed the volume.")
+            }
+        })
+    }
+}
+
+createChapters(copyDir)
