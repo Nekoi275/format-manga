b2e23559178f3190355f7f341748fd3a21b7dc66
nekoi <nekoi275@gmail.com>
2023-07-04 12:32:15 +0300

    bug fixes

--- formatManga.js
+++ formatManga.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const path = require('path');
 
-const MANGAFOLDER = '../../Manga';
-const MANGA = '';
-const COPYDIR = `${MANGAFOLDER}/`;
+const MANGAFOLDER = '../../Reading';
+const MANGA = 'claymore(manga)';
+const COPYDIR = `${MANGAFOLDER}/Claymore`;
 
 function createChapters(dir) {
     let volumes = fs.readdirSync(dir, { withFileTypes: true });
@@ -10,14 +11,15 @@ function createChapters(dir) {
     for (let i = 0; i < volumes.length; i++) {
         let chapters = fs.readdirSync(`${dir}/${volumes[i].name}`, { withFileTypes: true });
         for (let j = 0; j < chapters.length; j++) {
-            let isArchive = chapters[j].name.split('.')[1] === 'rar' || chapters[j].name.split('.')[1] === 'zip';
-            let isChapter = fs.lstatSync(`${dir}/${volumes[i].name}/${chapters[j].name}`).isDirectory();
+            let chapterName = `${dir}/${volumes[i].name}/${chapters[j].name}`;
+            let isArchive = path.extname(chapterName).toLowerCase() === '.rar' || path.extname(chapterName).toLowerCase() === '.zip';
+            let isChapter = fs.lstatSync(chapterName).isDirectory();
             if (!isChapter && !isArchive) {
                 console.log(`${volumes[i].name} is not a volume folder.`)
                 break;
             }
-            let newName = isArchive ? `${dir}/ch ${chapterNumber}.${chapters[j].name.split('.')[1]}` : `${dir}/ch ${chapterNumber}`;
-            fs.rename(`${dir}/${volumes[i].name}/${chapters[j].name}`, newName, (err) => {
+            let newName = isArchive ? `${dir}/ch ${chapterNumber}${path.extname(chapterName).toLowerCase()}` : `${dir}/ch ${chapterNumber}`;
+            fs.rename(chapterName, newName, (err) => {
                 if (err) {
                     console.log(err)
                 } else {
